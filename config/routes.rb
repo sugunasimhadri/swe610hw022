e3e7e0e6bb3b561b9f89d1a71ef471b040d113a7
Suguna Simhadri <simhadrisugun716@students.itu.edu>
2017-04-24 10:58:54 +0000

    I'M DONE

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  #resources :dogs
+
+  root "dogs#index"
+  get "dogs/new", to: "dogs#new"
+  get "dogs/:id", to: "dogs#show", as: :dog
+  get "dogs", to: "dogs#index"
+  post "dogs", to: "dogs#create"
+    
   get "users", to: "users#index"
   get "users/new", to: "users#new"
   get "users/:id", to: "users#show", as: :user
